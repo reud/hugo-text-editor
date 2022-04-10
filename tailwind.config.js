a75e471d9986ec2e453b3eef673e0b5018f4f8f7
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2022-04-10 09:03:24 +0900

    delete tailwind and update package.json

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-const { config } = require('@charcoal-ui/tailwind-config')
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  presets: [config],
-}
