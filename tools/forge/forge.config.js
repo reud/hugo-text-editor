98e15d4b0c02b6748aaa898126dc844d0e87a3a6
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2022-06-07 16:48:46 +0900

    update forge config

--- tools/forge/forge.config.js
+++ tools/forge/forge.config.js
@@ -89,7 +89,7 @@ module.exports = {
   publishers: [
     {
       name: "@electron-forge/publisher-github",
-      platforms: ["arm64"],
+      platforms: ["darwin","linux"],
       config: {
         repository: {
           owner: "reud",
